4e622e2b7c295654be0475f17f1a953483451cb2
Jade Beresford <jade_fletcher@hotmail.co.uk>
2020-02-03 14:17:22 +0000

    feat: report takes 2 greens

--- lib/reports.rb
+++ lib/reports.rb
@@ -1,5 +1,6 @@
 def report(grades)
   return "Amber: 1" if grades == 'a'
   return "Red: 1" if grades == 'r'
+  return "Green: 2" if grades == 'g, g'
   "Green: 1"
 end
--- spec/reports_spec.rb
+++ spec/reports_spec.rb
@@ -13,4 +13,8 @@ describe '.report()' do
   it 'returns 1 red when given a red' do
     expect(report('r')).to eq "Red: 1"
   end
+
+  it 'returns 2 green when given 2 green' do
+    expect(report('g, g')).to eq "Green: 2"
+  end
 end
